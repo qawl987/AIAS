8fb74fbd0c18f2b53f142151436a7fda2e595c19
qawl987 <shiny861216@gmail.com>
2024-09-29 18:35:09 +0000

    chore: delete unneed file

--- hw2-4/lazy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp ../t.cpp ./a.cc
-clear
-cd ./build
-# cmake -DCMAKE_PREFIX_PATH=./libtorch ..
-# cmake --build . --config Release -j ${nproc}
-cmake -DCMAKE_PREFIX_PATH=./libtorch -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build . --config Debug -j ${nproc}
