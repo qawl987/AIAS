d7e2d3bde2c1aea50e8fe18cbb0584ae2b514183
qawl987 <qawl987.cs13@nycu.edu.tw>
2024-11-03 10:43:52 +0000

    feat: hw2-3 other module

--- src/main/scala/acal_lab05/Hw2/CalStack.scala
+++ src/main/scala/acal_lab05/Hw2/CalStack.scala
@@ -10,6 +10,7 @@ class CalStack(val depth: Int) extends Module {
     val push    = Input(Bool())
     val cal     = Input(Bool())
     val dataIn  = Input(UInt(32.W))
+    val init = Input(Bool())
     val dataOutLast = Output(UInt(32.W))
     val dataOutSecond = Output(UInt(32.W))
     val empty   = Output(Bool())
@@ -19,6 +20,12 @@ class CalStack(val depth: Int) extends Module {
   val sp        = RegInit(0.U(log2Ceil(depth+1).W))
   val outLast       = RegInit(0.U(32.W))
   val outSecondLast = RegInit(0.U(32.W))
+
+  when(io.init){
+    sp := 0.U
+    outLast := 0.U
+    outSecondLast := 0.U
+  }
 
   when (io.en) {
     when(io.push && (sp < depth.asUInt)) {
@@ -45,48 +52,3 @@ class CalStack(val depth: Int) extends Module {
   io.dataOutSecond := outSecondLast
   io.empty   := sp === 0.U
 }
-
-
-// // See LICENSE.txt for license details.
-// package acal_lab05.Hw2
-
-// import chisel3._
-// import chisel3.util.log2Ceil
-
-// class CalStack(val depth: Int) extends Module {
-//   val io = IO(new Bundle {
-//     val en      = Input(Bool())
-//     val push    = Input(Bool())
-//     val cal     = Input(Bool())
-//     val dataIn  = Input(UInt(32.W))
-//     val dataOutLast = Output(UInt(32.W))
-//     val dataOutSecond = Output(UInt(32.W))
-//     val empty   = Output(Bool())
-//   })
-
-//   val stack_mem = Mem(depth, UInt(32.W))
-//   val sp        = RegInit(0.U(log2Ceil(depth+1).W))
-//   val outLast       = RegInit(0.U(32.W))
-//   val outSecondLast = RegInit(0.U(32.W))
-
-//   when (io.en) {
-//     when(io.push && (sp < depth.asUInt)) {
-//       stack_mem(sp) := io.dataIn
-//       sp := sp + 1.U
-//     } .elsewhen(io.cal && (sp > 0.U)) {
-//         stack_mem(sp - 2.U) := io.dataIn
-//         sp := sp - 1.U
-//     }
-//     when (sp > 0.U) {
-//       outLast := stack_mem(sp - 1.U)
-//     }
-//     when(sp > 1.U) {
-//         outSecondLast := stack_mem(sp - 2.U)
-//     }
-//   }
-
-//   io.dataOutLast := outLast
-//   io.dataOutSecond := outSecondLast
-//   io.empty   := sp === 0.U
-// }
-
--- src/main/scala/acal_lab05/Hw2/OneClockStack.scala
+++ src/main/scala/acal_lab05/Hw2/OneClockStack.scala
@@ -8,6 +8,7 @@ class OneClockStack[T <: Data](val dataType: T, val depth: Int) extends Module {
     val pop     = Input(Bool())
     val en      = Input(Bool())
     val dataIn  = Input(dataType)
+    val init    = Input(Bool())
     val dataOut = Output(dataType)
     val empty   = Output(Bool())
   })
@@ -23,6 +24,10 @@ class OneClockStack[T <: Data](val dataType: T, val depth: Int) extends Module {
   val curr_top_ptr = count_reg - 1.U
   val next_top_ptr = count_reg - 2.U
   
+  when(io.init){
+    count_reg := 0.U
+    dataOut_reg := 0.U
+  }
   // Output logic - update dataOut_reg on clock edge
   when (io.en && io.pop && (count_reg > 0.U)) {
     // If popping, show the next item in stack (or default if empty)
@@ -52,57 +57,3 @@ class OneClockStack[T <: Data](val dataType: T, val depth: Int) extends Module {
     }
   }
 }
-// package acal_lab05.Hw2 
-// import chisel3._
-// import chisel3.util._
-
-// class OneClockStack(val depth: Int) extends Module {
-//   val io = IO(new Bundle {
-//     val push    = Input(Bool())
-//     val pop     = Input(Bool())
-//     val en      = Input(Bool())
-//     val dataIn  = Input(UInt(32.W))
-//     val dataOut = Output(UInt(32.W))
-//     val empty   = Output(Bool())
-//   })
-  
-//   // Internal storage
-//   val stack_mem = Reg(Vec(depth, UInt(32.W)))
-//   val count_reg = RegInit(0.U(log2Ceil(depth + 1).W))
-  
-//   // Register for data output to avoid combinational loops
-//   val dataOut_reg = RegInit(0.U(32.W))
-  
-//   // Calculate top pointers
-//   val curr_top_ptr = count_reg - 1.U
-//   val next_top_ptr = count_reg - 2.U
-  
-//   // Output logic - update dataOut_reg on clock edge
-//   when (io.en && io.pop && (count_reg > 0.U)) {
-//     // If popping, show the next item in stack (or 0 if empty)
-//     dataOut_reg := Mux(count_reg > 1.U, stack_mem(next_top_ptr), 0.U)
-//   } .elsewhen (count_reg > 0.U) {
-//     // If not popping, show the current top
-//     dataOut_reg := stack_mem(curr_top_ptr)
-//   } .otherwise {
-//     dataOut_reg := 0.U
-//   }
-
-//   // Connect dataOut to registered dataOut_reg
-//   io.dataOut := dataOut_reg
-  
-//   // Empty signal
-//   io.empty := count_reg === 0.U
-  
-//   // State update logic
-//   when (io.en) {
-//     when (io.push && (count_reg < depth.U)) {
-//       // Push data onto stack and increment count
-//       stack_mem(count_reg) := io.dataIn
-//       count_reg := count_reg + 1.U
-//     } .elsewhen (io.pop && (count_reg > 0.U)) {
-//       // Pop from stack and decrement count
-//       count_reg := count_reg - 1.U
-//     }
-//   }
-// }
